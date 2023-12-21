02aa17da8e490d840c3bf3c900802898e0bd6bac
ACESTANOL <122461731+ACESTANOL@users.noreply.github.com>
2023-12-21 13:36:54 +0800

    added TC 12 & 13
    added TC 12 & 13

--- Test/TestCases/TC012.cs
+++ Test/TestCases/TC012.cs
@@ -1,12 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using SeleniumFramework.Pages;
+using SeleniumFramework.Utility;
+
 
 namespace SeleniumFramework.Test.TestCases
 {
     internal class TC012
     {
+        private IWebDriver _driver;
+
+        public TC012(IWebDriver driver)
+        {
+            _driver = driver;
+            TC012_Verify_View_Products_Details();
+        }
+
+        public void TC012_Verify_View_Products_Details()
+        {
+            Helpers helpers = new Helpers(_driver);
+            ProductsPage productsPage = new ProductsPage(_driver);
+
+            productsPage.isSVisibleProductText();
+            Thread.Sleep(1000);
+            helpers.ScrollDown(_driver, 2000);
+            productsPage.ViewProducts();
+
+            bool productName = productsPage.isSVisibleProductName();
+            bool productAmount = productsPage.isSVisibleProductAmount();
+            bool productQty = productsPage.isSVisibleProductQty();
+            bool productAvailability = productsPage.isSVisibleProductAvailability();
+            bool productCondition = productsPage.isSVisibleProductCondition();
+            bool producBrand = productsPage.isSVisibleProductBrand();
+
+            Assert.Multiple(() =>
+            {
+                // Add  assertions 
+                Assert.That(productName, Is.True, "Amount is visible.");
+                Assert.That(productAmount, Is.True, "Amount field is visible.");
+                Assert.That(productQty, Is.True, "Quantity is visible.");
+                Assert.That(productCondition, Is.True, "Conditions is visble.");
+                Assert.That(producBrand, Is.True, "Brand is visble.");
+            });
+
+
+        }
+
+
+
+
+
     }
 }
--- Test/TestCases/TC013.cs
+++ Test/TestCases/TC013.cs
@@ -1,12 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using OpenQA.Selenium;
+using SeleniumFramework.Utility;
+using SeleniumFramework.Pages;
 
 namespace SeleniumFramework.Test.TestCases
 {
     internal class TC013
     {
+        private IWebDriver _driver;
+
+        public TC013(IWebDriver driver)
+        {
+            _driver = driver; ;
+            TC013_Verify_Feedack_Review_Form();
+        }
+
+        public void TC013_Verify_Feedack_Review_Form()
+        {
+            Helpers helpers = new Helpers(_driver);
+            ProductsPage productsPage = new ProductsPage(_driver);
+
+            productsPage.ReviewName();
+            productsPage.ReviewEmail();
+            productsPage.ReviewFeedbackInput();
+            productsPage.SubmitReview();
+
+        }
     }
 }
